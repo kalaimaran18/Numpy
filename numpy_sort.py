1c674580cc77dfd544cb04d398d7ea1f91edf38e
kalaimaran18 <kalaimaranr18@gmail.com>
2025-08-05 15:24:29 +0530

    Add files via upload

---
+++ numpy_sort.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# numpy using sort()method
+
+k=np.array([2,4,8,4,9,1,0])
+print(np.sort(k))
+
+#numpy using sort() metod in alphabetic
+
+j=np.array(["british","india","canada","australia","dubai"])
+print(np.sort(j))
+
+#numpy using sort() method in boolean
+
+v=np.array(["true","false","true","false"])
+print(np.sort(v))
+
+#numpy using sort() method in 2D array
+
+s=np.array([[4,6,1],[9,4,2]])
+print(np.sort(s))
+
+#numpy using sort() method in 3D
+
+l=np.array([[7,2,5],[6,4,2],[9,2,5]]),([[9,64,6],[0,6,2],[5,9,1]])
+print(np.sort(l))
+
+#numpy using sort() method in alpha numeric
+
+c=np.array(["tcs",23,"microsoft",78,"infotech",0])
+print(np.sort(c))
+
+#numpy using in sort() method in append
+
+e=np.array([0,10])
+print(np.append(e,45))
